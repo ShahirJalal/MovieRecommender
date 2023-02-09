b8eac153d351a841019b8ba930e727d7705c2e82
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-09 18:28:05 +0800

    update

---
+++ frontend/frontend/src/components/AddUserComponent.js
@@ -0,0 +1,105 @@
+import React,{useState,useEffect} from 'react'
+import UserService from '../services/UserService'
+import {Link, useNavigate, useParams }from 'react-router-dom'
+const AddUserComponet=()=>{
+    const [email, setEmail] = useState('')
+    const [userName, setUserName] = useState('')
+    const [userPassword, setUserPassword] = useState('')
+    const navigate=useNavigate();
+    const {userId}=useParams();
+    
+    const saveOrUpdateUser=(e)=>{
+       e.preventDefault();
+
+        const user={userId,email,userName,userPassword}
+        console.log(userId);//validate data
+        if(userId){
+            UserService.updateUser(userId,user).then((response)=>{
+                console.log(response.data) 
+                navigate('/users')                
+            }).catch(error=>{
+                console.log(error)
+            })
+
+        }else{
+            UserService.createUser(user).then((response)=>{
+                console.log(response.data)
+                navigate('/users')
+            }).catch(error=>{
+                console.log(error)
+            })
+        }
+
+    }
+    useEffect(() => {
+        UserService.getUserbyId(userId).then((response)=>{
+            console.log(response.data.email)
+            setEmail(response.data.email)
+            setUserName(response.data.userName)
+            setUserPassword(response.data.userPassword)
+        }).catch(error=>{
+        console.log(error)
+    })
+    }, [])
+
+    const title= () => {
+        if(userId){
+            return <h2 className = "text-center"> Update User</h2>
+        }else{
+            return <h2 className = "text-center"> Add User</h2>
+        }
+    }
+    return(
+        <div>
+            <br></br>
+            <div className = "container">
+                <div className= "row">
+                    <div className = "card col-md-6 offset-md-3 offset-md-3">
+                      {title()}
+                        <div className= "card-body">
+                            <form>
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Email  :</label>
+                                    <input
+                                            type="email"
+                                            placeholder="Enter Email"
+                                            name = "Email"
+                                            className="form-control"
+                                            value = {email}
+                                            onChange ={(e) => setEmail(e.target.value)}>
+                            </input>
+                        </div>
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Username :</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Enter username"
+                                        name = "username"
+                                        className="form-control"
+                                        value = {userName}
+                                        onChange ={(e) => setUserName(e.target.value)}
+                                        >
+                                    </input>
+                                </div>
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Password :</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Enter Password"
+                                        name = "userpassword"
+                                        className="form-control"
+                                        value = {userPassword}
+                                        onChange ={(e) => setUserPassword(e.target.value)}>
+                                    </input>
+                                </div>
+                             <button className="btn btn-success" onClick={(e)=>saveOrUpdateUser(e)}>Save User</button>
+                             <Link to="/users" className='btn btn-danger'>Cancel</Link>
+                        </form>
+                    </div>
+              </div>
+          </div>
+     </div>
+</div>
+    )
+}
+export default AddUserComponet
---
+++ frontend/frontend/src/components/ListMoviesComponent.js
@@ -0,0 +1,75 @@
+import React, {useState,useEffect} from 'react'
+import MovieService from '../services/MovieService'
+import Pagination from './Pagination'
+import { Link } from 'react-router-dom'
+import MoviePoster from './MoviePoster'
+
+const ListMovieComponent = () => {
+      const [movie, setMovie] = useState([]);
+      const [currentPage, setCurrentPage] = useState (1);
+      const [postsPerPage, setPostsPerPage] = useState (5)
+            useEffect(() => {
+                getMovies();
+            }, [])
+            const getMovies=()=> {
+                MovieService.getAllMovies().then((response)=> {
+                    setMovie(response.data)
+                    console.log(response.data);
+                }).catch(error=>{
+                    console.log(error);
+                })
+            }
+            const deleteMovie=(movieId)=> {
+                MovieService.deleteMovie(movieId).then((response)=> {     
+                    getMovies();          
+                }).catch(error=>{
+                    console.log(error);
+                })
+            }
+         const lastPostIndex = currentPage*postsPerPage;
+        const firstPostIndex = lastPostIndex - postsPerPage;
+        const curentPosts=movie.slice(firstPostIndex,lastPostIndex)
+      return(
+      <div className = "container">
+        <br />
+           <h2 className = "text-center"> Movies </h2> 
+           <Link to = "/add-Movie" className="btn btn-primary mb-2"> Add New Movie </Link>
+           <table className="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th> Movie Id </th>
+                        <th> Poster </th>
+                        <th> Title </th> 
+                        <th> Genres </th> 
+                        <th> Function </th> 
+                    </tr>
+                </thead>
+                  <tbody>
+                    {
+                        curentPosts.map( 
+                            movie =>
+                            <tr key = {movie.movieId}>
+                            <td> {movie.movieId} </td> 
+                            <td><MoviePoster movieId={movie.movieId}/></td>
+                            <td> {movie.title} </td> 
+                            <td> {movie.genres} </td>       
+                            <td>
+                                <Link className='btn btn-warning' to={`/edit-movie/${movie.movieId}`}>Update</Link>
+                                <button className='btn btn-danger' onClick={()=>deleteMovie(movie.movieId)} 
+                                style={{marginLeft:"10px", marginRight: "10px"}}>Delete</button>
+                                <Link className='btn btn-success' to={`/recommendations/${movie.movieId}`}>Recommendation</Link>
+                            </td>                      
+                            </tr>
+                    )
+                    }
+                  </tbody>
+           </table>
+           <Pagination 
+                    totalPosts={movie.length}
+                    postsPerpage={postsPerPage}
+                    setCurrentPage={setCurrentPage}>
+                    </Pagination>
+       </div>
+               )
+}
+export default ListMovieComponent
