127ea59976c60874b117bdede59bca52a42b8585
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-13 05:15:51 +0800

    updae

--- frontend/src/components/CustomHeader.js
+++ frontend/src/components/CustomHeader.js
@@ -14,7 +14,7 @@ const CustomHeader = () => {
                 <nav className = "navbar navbar-expand-md navbar-dark bg-dark"> 
                     <div>
                         <a href="http://localhost:3000/" className = "navbar-brand header-text"> 
-                            Movie Application
+                            <img src="https://raw.githubusercontent.com/ShahirJalal/ReferencePoint/a8fbc0f807b8dd8fc16dca0cb14c59084b0d6bf1/images/filmfrenzy-low-resolution-logo-color-on-transparent-background.svg" alt="Movie Application Logo" height="30" style={{marginRight: "10px"}} />
                         </a>
                     </div>
                     <div className="collapse navbar-collapse">
