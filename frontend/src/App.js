74860a16162ad85dbaa0cf23888e84189d569695
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-19 22:23:42 +0800

    Format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CustomFooter from './components/CustomFooter';
@@ -6,7 +7,7 @@ import ListUserComponent from './components/UsersList';
 import ListMoviesComponent from './components/MoviesList';
 import AddUserComponent from './components/AddUser';
 import AddMovieComponent from './components/AddMovie';
-import RecommendComponent from './components/RecommendationsList'
+import RecommendComponent from './components/RecommendationsList';
 import Login from './components/Login';
 import Registration from './components/Registration';
 import UserHome from './components/UserHome';
@@ -14,36 +15,35 @@ import UserMovies from './components/UserMovies';
 import AdminHome from './components/AdminHome';
 import FavouritesList from './components/FavouritesList';
 import ViewMovieComponent from './components/ViewMovie';
-import React from 'react';
 
 function App() {
   return (
-    <div >
+    <div>
       <BrowserRouter>
-      <CustomHeader></CustomHeader>
-      <div className="container">
-        <Routes>
-            <Route path = "/" element={<Login />}></Route>
-            <Route path = "/login" element={<Login />}></Route>
-            <Route path = "/registration" element={<Registration />}></Route>
+        <CustomHeader />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
 
-            <Route path= "/user-home" element={<UserHome />}></Route>
-            <Route path = "/user-movies" element={<UserMovies/>}></Route>
+            <Route path="/user-home" element={<UserHome />} />
+            <Route path="/user-movies" element={<UserMovies />} />
 
-            <Route path = "/recommendations/:movieId" element={<RecommendComponent/>}></Route>
-            <Route path= "/favourites" element={<FavouritesList />}></Route>
-            <Route path = "/view-movie/:movieId" element={<ViewMovieComponent/>}></Route>
+            <Route path="/recommendations/:movieId" element={<RecommendComponent />} />
+            <Route path="/favourites" element={<FavouritesList />} />
+            <Route path="/view-movie/:movieId" element={<ViewMovieComponent />} />
 
-            <Route path= "/admin-home" element={<AdminHome />}></Route>
-            <Route path = "/admin-movies" element={<ListMoviesComponent/>}></Route>
-            <Route path = "/add-Movie" element={<AddMovieComponent/>}></Route>
-            <Route path = "/edit-movie/:movieId" element={<AddMovieComponent/>}></Route>
-            <Route path = "/users" element={<ListUserComponent/>}></Route>
-            <Route path = "/add-User" element={<AddUserComponent/>}></Route>
-            <Route path = "/edit-user/:userId" element={<AddUserComponent/>}></Route>
-        </Routes>
-      </div>
-      <CustomFooter></CustomFooter>
+            <Route path="/admin-home" element={<AdminHome />} />
+            <Route path="/admin-movies" element={<ListMoviesComponent />} />
+            <Route path="/add-Movie" element={<AddMovieComponent />} />
+            <Route path="/edit-movie/:movieId" element={<AddMovieComponent />} />
+            <Route path="/users" element={<ListUserComponent />} />
+            <Route path="/add-User" element={<AddUserComponent />} />
+            <Route path="/edit-user/:userId" element={<AddUserComponent />} />
+          </Routes>
+        </div>
+        <CustomFooter />
       </BrowserRouter>
     </div>
   );
