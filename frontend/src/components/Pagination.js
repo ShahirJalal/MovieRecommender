1bf67eb914c1ee7c2975f2b81b9445036a0f044f
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-20 21:36:27 +0800

    comment Pagination.js

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+// Takes in total number of posts, number of posts per page and a setCurrentPage function
 const Pagination = ({ totalPosts, postsPerpage, setCurrentPage }) => {
   let pages = [];
+
+  // Loop for all pages
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerpage); i++) {
     pages.push(i);
   }
+
+  // Pagination component
   return (
     <div>
       {pages.map((page, index) => {
--- frontend/src/components/passwordUtils.js
+++
@@ -1,7 +0,0 @@
-import { createHash } from "crypto";
-
-export function hashPassword(password) {
-  const hash = createHash("sha256");
-  hash.update(password);
-  return hash.digest("hex");
-}
