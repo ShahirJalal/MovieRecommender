d72d77bb6c4e602b5510f8151abc80d9e82ec593
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-20 20:10:02 +0800

    comment MovieRating.js

--- frontend/src/components/MovieRating.js
+++ frontend/src/components/MovieRating.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MovieService from "../services/MovieService";
 
+// Display movie rating by movieId
 const useMovieRating = (movieId) => {
   const [rating, setRating] = useState(0);
   const [tmdbId, setTmdbId] = useState("");
 
+  // Get tmdbId by movieId
   useEffect(() => {
     MovieService.getLinksFromMovie(movieId)
       .then((response) => {
@@ -16,17 +18,18 @@ const useMovieRating = (movieId) => {
       });
   }, []);
 
+  // Get rating using TMDB API
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         `https://api.themoviedb.org/3/movie/${tmdbId}?api_key=3fd2be6f0c70a2a598f084ddfb75487c&language=en-US`
       );
-      setRating(result.data.vote_average / 2);
+      setRating(result.data.vote_average / 2); // Convert 10 star to 5 star rating system
     };
     fetchData();
   }, [tmdbId]);
 
-  return <label className="form-label">Rating: {rating.toFixed(1)}</label>;
+  return <label className="form-label">Rating: {rating.toFixed(1)}</label>; // Movie rating component
 };
 
 const MovieRating = ({ movieId }) => {
