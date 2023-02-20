3a32fcea8bece2c4c297abf348ce670213420078
Shahir Jalal <shahirjalalhazli@gmail.com>
2023-02-20 19:32:56 +0800

    comment Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { hashPassword } from "./PasswordUtils";
 
+// check user's role
 const withRoleCheck = (Component) => {
   return () => {
     const role = localStorage.getItem("role");
@@ -26,19 +27,21 @@ const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const hashedPassword = await hashPassword(password);
-      const response = await axios.post(
+      const hashedPassword = await hashPassword(password); // Hash the password
+      const response = await axios.post( // Sent for verification
         "http://localhost:8080/api/v1/users/login",
         {
           userName: username,
           userPassword: hashedPassword,
         }
       );
+
+      // Stores user's data in local storage
       if (response.data.role === "user") {
         localStorage.setItem("userId", response.data.userId);
         localStorage.setItem("userName", response.data.userName);
         localStorage.setItem("role", "user");
-        window.location.href = "http://localhost:3000/user-home";
+        window.location.href = "http://localhost:3000/user-home"; // Sends user to appropriate home page
       } else if (response.data.role === "admin") {
         localStorage.setItem("userId", response.data.userId);
         localStorage.setItem("userName", response.data.userName);
